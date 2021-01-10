e875375e69722c3ce4c75081a77956ae85ca5a51
muralivijay <muralidharan9845@gmail.com>
2021-01-10 20:23:20 +0000

    a2010:rootdir: Fix copying ueventd.mt6735.rc
    Signed-off-by: muralivijay <muralidharan9845@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -99,7 +99,6 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE           := ueventd.mt6735.rc
-LOCAL_MODULE_STEM      := ueventd.rc
 LOCAL_MODULE_TAGS      := optional eng
 LOCAL_MODULE_CLASS     := ETC
 LOCAL_SRC_FILES        := ueventd.mt6735.rc
