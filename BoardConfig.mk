b899b1cdeb439e1e8bda4234904dfae0d37434d6
muralivijay <muralidharan9845@gmail.com>
2021-01-10 20:30:29 +0000

    a2010: Fix zygote overriding
    * When build 32-bit build use zygote32. instaed of zygote64_32
    * No need copy manually init.zygote64_32.rc from system/core.
    
    Signed-off-by: muralivijay <muralidharan9845@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -152,9 +152,8 @@ TARGET_LDPRELOAD += libmtk_symbols.so
 TARGET_PROVIDES_INIT_RC := true
 
 # Zygote
-ifneq ($(FORCE_32_BIT),yes)
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.zygote=zygote32
+ifeq ($(FORCE_32_BIT),true)
+ADDITIONAL_DEFAULT_PROPERTIES += ro.zygote=zygote32
 else
-PRODUCT_COPY_FILES += system/core/rootdir/init.zygote64_32.rc:root/init.zygote64_32.rc
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.zygote=zygote64_32
+ADDITIONAL_DEFAULT_PROPERTIES += ro.zygote=zygote64_32
 endif
