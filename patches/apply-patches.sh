b1f6750f2bee9668ebf1a752fb6c33251ccbeb45
muralivijay <muralidharan9845@gmail.com>
2021-01-10 22:58:19 +0000

    a2010:Patches: update A2010-a >> a2010
    Signed-off-by: muralivijay <muralidharan9845@gmail.com>

--- patches/apply-patches.sh
+++ patches/apply-patches.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 cd ../../../..
 cd frameworks/av
-git apply --ignore-space-change --ignore-whitespace -v ../../device/lenovo/A2010-a/patches/frameworks_av/0001-Disable-usage-of-get_capture_position.patch 
+git apply --ignore-space-change --ignore-whitespace -v ../../device/lenovo/a2010/patches/frameworks_av/0001-Disable-usage-of-get_capture_position.patch 
 cd ../..
 cd system/core
-git apply --ignore-space-change --ignore-whitespace -v ../../device/lenovo/A2010-a/patches/system_core/0001-Remove-CAP_SYS_NICE-from-surfaceflinger.patch
-git apply --ignore-space-change --ignore-whitespace -v ../../device/lenovo/A2010-a/patches/system_core/0001-fix-ifx-thermal-spaming.patch
-git apply --ignore-space-change --ignore-whitespace -v ../../device/lenovo/A2010-a/patches/system_core/0001-mt6735-init-patches.patch
-#git apply -v ../../device/lenovo/A2010-a/patches/system_core/0002-Changes-for-more-level-log.patch
+git apply --ignore-space-change --ignore-whitespace -v ../../device/lenovo/a2010/patches/system_core/0001-Remove-CAP_SYS_NICE-from-surfaceflinger.patch
+git apply --ignore-space-change --ignore-whitespace -v ../../device/lenovo/a2010/patches/system_core/0001-fix-ifx-thermal-spaming.patch
+git apply --ignore-space-change --ignore-whitespace -v ../../device/lenovo/a2010/patches/system_core/0001-mt6735-init-patches.patch
+#git apply -v ../../device/lenovo/a2010/patches/system_core/0002-Changes-for-more-level-log.patch
 cd ../..
 cd system/netd
-git apply --ignore-space-change --ignore-whitespace -v ../../device/lenovo/A2010-a/patches/system_netd/0001-Revert-Don-t-start-tethering-if-IPv6-RPF-is-not-supp.patch
-git apply --ignore-space-change --ignore-whitespace -v ../../device/lenovo/A2010-a/patches/system_netd/0002-Enable-Tethering.patch
+git apply --ignore-space-change --ignore-whitespace -v ../../device/lenovo/a2010/patches/system_netd/0001-Revert-Don-t-start-tethering-if-IPv6-RPF-is-not-supp.patch
+git apply --ignore-space-change --ignore-whitespace -v ../../device/lenovo/a2010/patches/system_netd/0002-Enable-Tethering.patch
 cd ../..
 cd packages/apps/Settings
-git apply --ignore-space-change --ignore-whitespace -v ../../../device/lenovo/A2010-a/patches/packages_app_Settings/author.patch
+git apply --ignore-space-change --ignore-whitespace -v ../../../device/lenovo/a2010/patches/packages_app_Settings/author.patch
 cd ../../../
 cd system/sepolicy
-git apply --ignore-space-change --ignore-whitespace -v ../../device/lenovo/A2010-a/patches/system_sepolicy/0001-Revert-back-to-version-29.patch
+git apply --ignore-space-change --ignore-whitespace -v ../../device/lenovo/a2010/patches/system_sepolicy/0001-Revert-back-to-version-29.patch
 cd ../..
 echo Patches Applied Successfully!
--- patches/check-patches.sh
+++ patches/check-patches.sh
@@ -1,20 +1,20 @@
 cd ../../../..
 cd frameworks/av
-git apply -v --check ../../device/lenovo/A2010-a/patches/frameworks_av/0001-Disable-usage-of-get_capture_position.patch
+git apply -v --check ../../device/lenovo/a2010/patches/frameworks_av/0001-Disable-usage-of-get_capture_position.patch
 cd ../..
 cd system/core
-git apply -v --check ../../device/lenovo/A2010-a/patches/system_core/0001-Remove-CAP_SYS_NICE-from-surfaceflinger.patch
-git apply -v --check ../../device/lenovo/A2010-a/patches/system_core/0001-fix-ifx-thermal-spaming.patch
-git apply -v --check ../../device/lenovo/A2010-a/patches/system_core/0001-mt6735-init-patches.patch
+git apply -v --check ../../device/lenovo/a2010/patches/system_core/0001-Remove-CAP_SYS_NICE-from-surfaceflinger.patch
+git apply -v --check ../../device/lenovo/a2010/patches/system_core/0001-fix-ifx-thermal-spaming.patch
+git apply -v --check ../../device/lenovo/a2010/patches/system_core/0001-mt6735-init-patches.patch
 cd ../..
 cd system/netd
-git apply -v --check ../../device/lenovo/A2010-a/patches/system_netd/0001-Revert-Don-t-start-tethering-if-IPv6-RPF-is-not-supp.patch
-git apply -v --check ../../device/lenovo/A2010-a/patches/system_netd/0002-Enable-Tethering.patch
+git apply -v --check ../../device/lenovo/a2010/patches/system_netd/0001-Revert-Don-t-start-tethering-if-IPv6-RPF-is-not-supp.patch
+git apply -v --check ../../device/lenovo/a2010/patches/system_netd/0002-Enable-Tethering.patch
 cd ../..
 cd packages/apps/Settings
-git apply -v --check ../../../device/lenovo/A2010-a/patches/packages_app_Settings/author.patch
+git apply -v --check ../../../device/lenovo/a2010/patches/packages_app_Settings/author.patch
 cd ../../../
 cd system/sepolicy
-git apply -v --check ../../device/lenovo/A2010-a/patches/system_sepolicy/0001-Revert-back-to-version-29.patch
+git apply -v --check ../../device/lenovo/a2010/patches/system_sepolicy/0001-Revert-back-to-version-29.patch
 cd ../..
 echo Patches Applied Successfully!
